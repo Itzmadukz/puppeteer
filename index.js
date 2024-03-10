991280bde89f402b1defa759ac91f7cd0641a7b8
Christopher Maduka <61519473+Itzmadukz@users.noreply.github.com>
2024-03-10 17:22:55 -0400

    ADD: sample item price extraction

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer');
 
+(async () => {
+
+    const browser = await puppeteer.launch({ headless: false })
+    const page = await browser.newPage()
+
+    await page.goto('https://ebay.ca')
+    await page.type('#gh-ac', 'macbook pro');
+    await page.click('input[value="Search"]')
+
+    await page.waitForSelector('span.s-item__price')
+
+       console.log( await page.$$eval('span.s-item__price', spans => { 
+            return [...spans].map(span => { 
+                return span.innerText;
+            })
+        }))
+})()
