215edcc33884fe348802ae4797375bb191948246
Christopher Maduka <61519473+Itzmadukz@users.noreply.github.com>
2024-03-11 19:52:05 -0400

    Add: price range extraction \t code refactor for readability

--- index.js
+++ index.js
@@ -1,19 +1,70 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
+
+// test retailer data
+const ebayData = {
+    url: 'https://ebay.ca',
+    searchSelector: '#gh-ac',
+    clickSelector: 'input[value="Search"]',
+    priceSelector: 'span.s-item__price'
+}
+
+const extractAndTrimNumbers = dataArray => {
+    let lowest = null;
+    let highest = null;
+
+    dataArray.forEach(item => {
+        if (item !== '$20.00') {
+            const matches = item.match(/[\d,.]+/g);
+            if (matches) {
+                const trimmedValues = matches.map(match => parseFloat(match.replace(/,/g, '')));
+
+                // Update lowest and highest values
+                if (trimmedValues.length > 0) {
+                    const currentLowest = Math.min(...trimmedValues);
+                    const currentHighest = Math.max(...trimmedValues);
+
+                    if (lowest === null || currentLowest < lowest) {
+                        lowest = currentLowest;
+                    }
+
+                    if (highest === null || currentHighest > highest) {
+                        highest = currentHighest;
+                    }
+                }
+            }
+        }
+    });
+
+    return [lowest, highest];
+}
+
+const crawlRetailer = async ({ url, searchSelector, clickSelector, priceSelector }) => {
 
     const browser = await puppeteer.launch({ headless: false })
     const page = await browser.newPage()
 
-    await page.goto('https://ebay.ca')
-    await page.type('#gh-ac', 'macbook pro');
-    await page.click('input[value="Search"]')
+    try {
+        await page.goto(url)
+        await page.type(searchSelector, '16-inch Macbook Pro 2023');
+        await page.click(clickSelector)
 
-    await page.waitForSelector('span.s-item__price')
+        await page.waitForSelector(priceSelector)
 
-       console.log( await page.$$eval('span.s-item__price', spans => { 
-            return [...spans].map(span => { 
-                return span.innerText;
+        const prices = await page.$$eval(priceSelector, elements => {
+            return [...elements].map(element => {
+                return element.innerText;
             })
-        }))
-})()
+        })
+
+        console.log(prices)
+        console.log("==========================")
+        console.log(extractAndTrimNumbers(prices))
+    } catch (error) {
+        console.log("Error Occured", error)
+
+    }
+
+}
+
+console.log(crawlRetailer(ebayData))
